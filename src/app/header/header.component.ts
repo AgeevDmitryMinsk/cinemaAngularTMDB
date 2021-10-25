b9413490e29969aa738567d360472e9a0dcc9b79
Ageev Dmitry <51228454+AgeevDmitryMinsk@users.noreply.github.com>
2021-10-25 20:04:56 +0300

    add overview class to the video-card.component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    public person_account() {
+        alert(`переход в режим работы с аккаунтом`);
+    }
 
-  ngOnInit(): void {
-  }
+    public light_mode() {
+        alert(`режим "день"`);
+    }
+
+    public mode_night() {
+        alert(`режим "день"`);
+    }
+
+    public me() {
+        alert(`+375 29 616 40 88`);
+    }
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
